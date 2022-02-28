1bcb9f1d8f7c7db144c7bc1f4dd829af091c8a3d
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-28 13:52:13 +0530

    Added:Created a Address Book Spring Project.

---
+++ src/main/java/com/bridgelabz/addressbookbackendspring/AddressBookBackendSpringApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookBackendSpringApplication {
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(AddressBookBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/addressbookbackendspring/AddressBookBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
